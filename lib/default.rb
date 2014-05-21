07c482673ad42bb13c9f2ec4675f96c38381fcf8
Alexey Smirnov <alexey@itri.org.tw>
2014-05-21 09:03:21 +0800

    index.html (list of kafismas) generated automatically from content

--- lib/default.rb
+++ lib/default.rb
@@ -1,2 +1,17 @@
 # All files in the 'lib' directory will be loaded
 # before nanoc starts compiling.
+
+module PsaltirHelper
+
+    def kafismas
+        @items.select { |item| item[:kind] == 'kafisma' }
+    end
+
+    def sorted_kafismas
+        kafismas.sort_by { |p| p[:number] }
+    end
+
+end
+
+include PsaltirHelper
+
