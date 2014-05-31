2b3d28db2af5c16f1793a48afbf3013a5f7c42c6
Alexey Smirnov <alexey@itri.org.tw>
2014-05-31 21:26:28 +0800

    * Style of main view improved    * Options dialog GUI created

--- static/controllers.js
+++ static/controllers.js
@@ -1,6 +1,6 @@
 angular.module('psaltir.controllers', [])
 
-.controller('PsaltirCtrl', function($scope, $ionicActionSheet) {
+.controller('PsaltirCtrl', function($scope, $ionicActionSheet, $ionicModal) {
 
  $scope.show = function() {
 
@@ -12,11 +12,29 @@ angular.module('psaltir.controllers', [])
      ],
      destructiveText: 'Exit app',
      buttonClicked: function(index) {
+       $scope.modal.show();
        return true;
+
      }
    });
+};
 
- };
+    $ionicModal.fromTemplateUrl('modal.html', {
+      scope: $scope,
+      animation: 'slide-in-up'
+    }).then(function (modal) {
+      $scope.modal = modal;
+    });
+
+    // function to open the modal
+    $scope.openModal = function () {
+      $scope.modal.show();
+    };
+
+    // function to close the modal
+    $scope.closeModal = function () {
+      $scope.modal.hide();
+    };
 
 })
 
