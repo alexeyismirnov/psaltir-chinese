0a852d574428bd5a03494293ee76a49a23ef13ca
Alexey Smirnov <alexey@itri.org.tw>
2014-06-01 09:41:30 +0800

    A few popups added (about, confirmExit).

--- static/controllers.js
+++ static/controllers.js
@@ -1,21 +1,36 @@
 angular.module('psaltir.controllers', [])
 
-.controller('PsaltirCtrl', function($scope, $ionicActionSheet, $ionicModal) {
+.controller('PsaltirCtrl', function($scope, $ionicActionSheet, $ionicModal, $ionicPopup) {
 
  $scope.show = function() {
 
    // Show the action sheet
    $ionicActionSheet.show({
      buttons: [
-       { text: 'Options' },
+       { text: '选项' },
        { text: 'About this app' },
      ],
      destructiveText: 'Exit app',
      buttonClicked: function(index) {
-       $scope.modal.show();
-       return true;
+       switch (index) {
+       case 0:
+           $scope.modal.show();
+           break;
+       case 1:
+           $scope.about();
+           break;
 
+       default:
+           break;
+       }
+       return true;
+     },
+
+     destructiveButtonClicked: function() {
+       $scope.confirmExit();
+       return true;
      }
+
    });
 };
 
@@ -35,6 +50,28 @@ angular.module('psaltir.controllers', [])
     $scope.closeModal = function () {
       $scope.modal.hide();
     };
+
+
+    $scope.about = function() {
+      $ionicPopup.alert({
+        title: "О программе", 
+        template: "The app was created by Orthodox Brotherhood of Saint Apostles Peter and Paul in Hong Kong. Text of the Psalter was translated into Mandarin Chinese by Bishop Innokenty of Beijing and published by the Russian Mission (Beiguan) in 1910."
+      });
+
+    };
+
+    $scope.confirmExit = function() {
+      $ionicPopup.confirm({
+        title: "Exit app", 
+        template: "Do you want to exit app?",
+        cancelText: '取消'
+
+      }).then(function (res) {
+        if (res)
+          navigator.app.exitApp();
+      });
+    };
+
 
 })
 
